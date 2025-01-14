14a4da1a4d7b4f83690dd4ad711e3150bce95a7d
naterrie <nathanterrier38580@gmail.com>
2025-01-14 14:22:44 +0100

    removing useless variable

--- src/64bits.c
+++ src/64bits.c
@@ -3,10 +3,8 @@
 void nm64bits(struct nm *nm)
 {
 	Elf64_Ehdr	*hdr;
-	Elf64_Shdr	*shdr;
+
 	hdr = (Elf64_Ehdr *)nm->map;
-	shdr = (Elf64_Shdr *)(nm->map + hdr->e_shoff);
-	printf("hdr->e_ehsize: %ld\n", sizeof(Elf64_Ehdr));
 	if (hdr->e_ehsize != sizeof(Elf64_Ehdr))
 		print_error("file format not recognized", nm);
 
