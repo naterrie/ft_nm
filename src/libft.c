78e9e2f73138f50151b116958fd460f78ae734bc
naterrie <naterrie@student.42lyon.fr>
2025-09-30 13:07:30 +0200

    fix potential crash on NULL args

--- src/libft.c
+++ src/libft.c
@@ -28,10 +28,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (s1 == NULL || s2 == NULL)
+		return -1;
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (s1[i] && s1[i] == s2[i] && i < n - 1)
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return (s1[i] - s2[i]);
 }
@@ -42,6 +44,8 @@ char	*ft_strdup(const char *s)
 	char	*dup;
 	int		i;
 
+	if (s == NULL)
+		return NULL;
 	i = 0;
 	len = ft_strlen(s);
 	dup = malloc(len + 1);
